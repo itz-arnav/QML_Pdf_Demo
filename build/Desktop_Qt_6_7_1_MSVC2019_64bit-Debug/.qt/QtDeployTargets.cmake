d68864d4c61f703920dd419f4c58cca6ffc6f733
Arnab Kumar Jha <arnab.jha@enpass.io>
2024-06-06 16:00:53 +0530

    Stop tracking build directory and user-specific files

--- build/Desktop_Qt_6_7_1_MSVC2019_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_appPdfViewingDemo_FILE D:/Qt Learning With Projects/PdfViewingDemo/build/Desktop_Qt_6_7_1_MSVC2019_64bit-Debug/appPdfViewingDemo.exe)
-set(__QT_DEPLOY_TARGET_appPdfViewingDemo_RUNTIME_DLLS C:/Qt/6.7.1/msvc2019_64/bin/Qt6Quickd.dll;C:/Qt/6.7.1/msvc2019_64/bin/Qt6QmlModelsd.dll;C:/Qt/6.7.1/msvc2019_64/bin/Qt6Qmld.dll;C:/Qt/6.7.1/msvc2019_64/bin/Qt6Networkd.dll;C:/Qt/6.7.1/msvc2019_64/bin/Qt6OpenGLd.dll;C:/Qt/6.7.1/msvc2019_64/bin/Qt6Guid.dll;C:/Qt/6.7.1/msvc2019_64/bin/Qt6Cored.dll)
